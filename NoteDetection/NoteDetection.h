828a5657b923c7421dc7b65eed9826d490f128f6
rikner <rikner@web.de>
2017-03-24 09:16:22 +0100

    Initial Commit

---
+++ NoteDetection/NoteDetection.h
@@ -0,0 +1,19 @@
+//
+//  NoteDetection.h
+//  NoteDetection
+//
+//  Created by flowing erik on 24.03.17.
+//  Copyright © 2017 flowkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NoteDetection.
+FOUNDATION_EXPORT double NoteDetectionVersionNumber;
+
+//! Project version string for NoteDetection.
+FOUNDATION_EXPORT const unsigned char NoteDetectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoteDetection/PublicHeader.h>
+
+
