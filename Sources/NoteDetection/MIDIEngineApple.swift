3e175941f18b190bc410d7c17b8c5249006260c1
rikner <rikner@web.de>
2018-04-06 23:19:41 +0200

    Revert "call MIDIEndpointDispose() for each outConnection in disconnect()"
    This reverts commit e9bce62e22aa2ddde76a794b4ec81e1dd8f762f3.

--- Sources/NoteDetection/MIDIEngineApple.swift
+++ Sources/NoteDetection/MIDIEngineApple.swift
@@ -101,8 +101,6 @@ class MIDIEngine {
     }
 
     private func disconnect() {
-
-        // SOURCES
         for sourceIndex in 0 ..< MIDIGetNumberOfSources() {
             let source = MIDIGetSource(sourceIndex)
 
@@ -114,14 +112,8 @@ class MIDIEngine {
 
             try? MIDIPortDisconnectSource(inputPort, source).throwOnError()
         }
+
         midiDeviceList = []
-
-
-        // DESTINATIONS
-        for destinationIndex in 0 ..< MIDIGetNumberOfDestinations() {
-            let endpointRef = MIDIGetDestination(destinationIndex)
-            MIDIEndpointDispose(endpointRef)
-        }
         midiOutConnections = []
     }
 
