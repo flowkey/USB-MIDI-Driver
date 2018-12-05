839cc0a7ab7d98358a3163c5087b5a3913fa6990
rikner <rikner@web.de>
2018-12-05 17:49:46 +0100

    cleanup & lint new tests

--- NoteDetectionTests/AudioEngineTest.swift
+++ NoteDetectionTests/AudioEngineTest.swift
@@ -11,9 +11,8 @@ import Dispatch
 @testable import NoteDetection
 
 class AudioEngineIOSTests: XCTestCase {
-    
     var audioEngine = try! AudioEngine()
-    
+
     override func setUp() {
         audioEngine = try! AudioEngine()
     }
@@ -27,64 +26,76 @@ class AudioEngineIOSTests: XCTestCase {
     func testInputIsDisabled() {
         try! audioEngine.startMicrophone()
         try! audioEngine.stopMicrophone()
-        
+
         XCTAssertFalse(audioEngine.inputIsEnabled)
     }
-    
+
     func testIfOnAudioDataIsCalled() {
-        let onAudioDataCalled = XCTestExpectation(description: "onAudioData callback is called")
-        audioEngine.set(onAudioData:  { audioData, audioTime in
-            onAudioDataCalled.fulfill()
+        let onAudioDataWasCalled = XCTestExpectation(
+            description: "onAudioData callback is called"
+        )
+        audioEngine.set(onAudioData:  { _, _ in
+            onAudioDataWasCalled.fulfill()
         })
         try! audioEngine.startMicrophone()
-        wait(for: [onAudioDataCalled], timeout: 0.1)
+        wait(for: [onAudioDataWasCalled], timeout: 0.1)
     }
-    
+
     func testIfAudioDataIsNonZero() {
         let audioRuntimeDuration = 0.1
         var aggregatedAudioData: [Float] = []
-        let audioDataIsNonZero = XCTestExpectation(description: "collected audio data is non-zero")
-        audioEngine.set(onAudioData:  { audioData, audioTime in
+        let audioDataIsNonZero = XCTestExpectation(
+            description: "collected audio data is non-zero"
+        )
+        audioEngine.set(onAudioData:  { audioData, _ in
             aggregatedAudioData.append(contentsOf: audioData)
         })
-        
+
         try! audioEngine.startMicrophone()
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + audioRuntimeDuration, execute: {
-            try! self.audioEngine.stopMicrophone()
-            let rms = rootMeanSquare(aggregatedAudioData)
-            if rms > 0 {
-                audioDataIsNonZero.fulfill()
+        DispatchQueue.main.asyncAfter(
+            deadline: DispatchTime.now() + audioRuntimeDuration,
+            execute: {
+                try! self.audioEngine.stopMicrophone()
+                let rms = rootMeanSquare(aggregatedAudioData)
+                if rms > 0 {
+                    audioDataIsNonZero.fulfill()
+                }
             }
-        })
-        
+        )
+
         wait(for: [audioDataIsNonZero], timeout: audioRuntimeDuration + 0.1)
     }
-    
+
     func testIfAudioTimeIsInMilliseconds() {
         // the longer we test, the lower the accuracy parameter can be
         let audioTestDurationS = 2.0
         let accuracy = 0.01
-        let expectation = XCTestExpectation(description: "audio time is in milliseconds")
+        let audioTimeIsInMS = XCTestExpectation(
+            description: "audio time is in milliseconds"
+        )
         var audioTimes: [AudioTime] = []
         var referenceTimes: [Double] = []
-        
-        audioEngine.set(onAudioData:  { audioData, audioTime in
+
+        audioEngine.set(onAudioData:  { _, audioTime in
             audioTimes.append(audioTime)
             referenceTimes.append(Date().timeIntervalSince1970.toMilliseconds())
         })
-        
+
         try! audioEngine.startMicrophone()
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + audioTestDurationS, execute: {
-            try! self.audioEngine.stopMicrophone()
-            let audioTimeDiffMean = audioTimes.getDifferences().getMean()
-            let referenceTimeDiffMean = referenceTimes.getDifferences().getMean()
-            XCTAssertEqual(audioTimeDiffMean, referenceTimeDiffMean, accuracy: accuracy)
-            expectation.fulfill()
-        })
-        
-        wait(for: [expectation], timeout: audioTestDurationS + 1)
+        DispatchQueue.main.asyncAfter(
+            deadline: DispatchTime.now() + audioTestDurationS,
+            execute: {
+                try! self.audioEngine.stopMicrophone()
+                let audioTimeDiffMean = audioTimes.getDifferences().getMean()
+                let referenceTimeDiffMean = referenceTimes.getDifferences().getMean()
+                XCTAssertEqual(audioTimeDiffMean, referenceTimeDiffMean, accuracy: accuracy)
+                audioTimeIsInMS.fulfill()
+            }
+        )
+
+        wait(for: [audioTimeIsInMS], timeout: audioTestDurationS + 1)
     }
-    
+
     func testGetDifferences() {
         let arr: [Double] = [2, 4, 5, 10]
         XCTAssertEqual(arr.getDifferences(), [2, 1, 5])
@@ -108,10 +119,8 @@ fileprivate extension Array where Element == Double {
         }
         return diffs
     }
-    
+
     func getMean() -> Double {
         return self.reduce(0, +) / Double(self.count)
     }
 }
-
-
